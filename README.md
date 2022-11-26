f106428b317c059bae3a148aa92a8819a5b33bad
espenhalsen <espylv@gmail.com>
2022-11-26 03:00:17 +0100

    added readme

---
+++ README.md
@@ -0,0 +1,46 @@
+
+# HASK - Open Source Peer-to-Peer JS
+
+HASK uses JS and EJS to create a peer-to-peer connection. Web-camera is required. 
+
+
+## Run Locally on your machine:
+
+Clone the HASK project to your designated folder
+
+```bash
+  git clone https://github.com/espenhalsen/hask.git
+```
+
+Go to the cloned HASK directory
+
+```bash
+  cd hask
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the HASK server
+
+```bash
+  npm start
+```
+
+
+## Development
+
+To develope on the HASK project, use the command below
+
+```bash
+  npm run devStart
+```
+This will start a nodemon development server, which updates each time you save a file.
+
+## Authors
+
+- Espen Halsen - [@Halsen](https://www.github.com/espenhalsen)
+
---
+++ public/script.js
@@ -0,0 +1,149 @@
+const socket = io('/')
+const videoGrid = document.getElementById('video-grid')
+const myPeer = new Peer(undefined, {
+  path: '/peerjs',
+  host: '/',
+  port: '443'
+})
+let myVideoStream;
+const myVideo = document.createElement('video')
+myVideo.muted = true;
+const peers = {}
+navigator.mediaDevices.getUserMedia({
+  video: true,
+  audio: true
+}).then(stream => {
+  myVideoStream = stream;
+  addVideoStream(myVideo, stream)
+  myPeer.on('call', call => {
+    call.answer(stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+      addVideoStream(video, userVideoStream)
+    })
+  })
+
+  socket.on('user-connected', userId => {
+    connectToNewUser(userId, stream)
+  })
+  // input value
+  let text = $("input");
+  // when press enter send message
+  $('html').keydown(function (e) {
+    if (e.which == 13 && text.val().length !== 0) {
+      socket.emit('message', text.val());
+      text.val('')
+    }
+  });
+  socket.on("createMessage", message => {
+    $("ul").append(`<li class="message"><b>Swagman</b><br/>${message}</li>`);
+    scrollToBottom()
+  })
+})
+
+socket.on('user-disconnected', userId => {
+  if (peers[userId]) peers[userId].close()
+})
+
+myPeer.on('open', id => {
+  socket.emit('join-room', ROOM_ID, id)
+})
+
+function connectToNewUser(userId, stream) {
+  const call = myPeer.call(userId, stream)
+  const video = document.createElement('video')
+  call.on('stream', userVideoStream => {
+    addVideoStream(video, userVideoStream)
+  })
+  call.on('close', () => {
+    video.remove()
+  })
+
+  peers[userId] = call
+}
+
+function addVideoStream(video, stream) {
+  video.srcObject = stream
+  video.addEventListener('loadedmetadata', () => {
+    video.play()
+  })
+  videoGrid.append(video)
+}
+
+
+
+const scrollToBottom = () => {
+  var d = $('.main__chat_window');
+  d.scrollTop(d.prop("scrollHeight"));
+}
+
+
+const muteUnmute = () => {
+  const enabled = myVideoStream.getAudioTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getAudioTracks()[0].enabled = false;
+    setUnmuteButton();
+  } else {
+    setMuteButton();
+    myVideoStream.getAudioTracks()[0].enabled = true;
+  }
+}
+
+const playStop = () => {
+  console.log('object')
+  let enabled = myVideoStream.getVideoTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getVideoTracks()[0].enabled = false;
+    setPlayVideo()
+  } else {
+    setStopVideo()
+    myVideoStream.getVideoTracks()[0].enabled = true;
+  }
+}
+
+const setMuteButton = () => {
+  const html = `
+    <i class="fas fa-microphone"></i>
+    <span>Mute</span>
+  `
+  document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+const setUnmuteButton = () => {
+  const html = `
+    <i class="unmute fas fa-microphone-slash"></i>
+    <span>Unmute</span>
+  `
+  document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+const setStopVideo = () => {
+  const html = `
+    <i class="fas fa-video"></i>
+    <span>Hide Video</span>
+  `
+  document.querySelector('.main__video_button').innerHTML = html;
+}
+
+const setPlayVideo = () => {
+  const html = `
+  <i class="stop fas fa-video-slash"></i>
+    <span>Show Video</span>
+  `
+  document.querySelector('.main__video_button').innerHTML = html;
+}
+const changeRoom = () => {
+
+        var newroom=prompt('Enter new Room-ID');
+        if(newroom) location.assign(newroom);
+    }
+const disconnect = () => {
+      var disconnect=confirm("Are you sure you want to disconnect?");
+      if(disconnect) window.location.href = "https://halsendata.com";
+  }
+const shareRoom = () => {
+  
+        navigator.clipboard.writeText(window.location.href);
+        alert("Your room link has been copied to the clipboard!\n\nYour room link is:\n" + window.location.href + "\n\nPaste the link in a new browser, and enjoy! \n\nNOTE: If you use the 'Change Room' function, only include the ID in the link!");
+}
+
